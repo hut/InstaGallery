0e5a15d1a93ebd07664d70b974262647f1a0c4e8
hut <hut@hut.pm>
2023-06-17 01:06:33 +0200

    remove javascript (and some unused CSS)

--- index.php
+++ index.php
@@ -355,22 +355,6 @@ img.thumbnail {
     max-width: calc(100% - 30px);
 }
 
-#ctrlbox {
-    z-index: 1000;
-    position: absolute;
-    top: 0;
-    right: 50px;
-    height: 50px;
-    width: 120px;
-    text-align: right;
-    color: white;
-    font: larger bold;
-    cursor: pointer;
-}
-
-#ctrlbox i {
-    padding: 15px;
-}
 #footer {
     margin: 30px;
 }
@@ -380,29 +364,6 @@ img.thumbnail {
     color: #555;
     font-weight: bold;
 }
-
-select {
-    padding:3px;
-    margin: 0;
-    -webkit-border-radius:4px;
-    -moz-border-radius:4px;
-    border-radius:4px;
-    border: solid 1px #999;
-    background: #fff;
-    color:#000;
-    outline:none;
-    display: inline-block;
-    cursor:pointer;
-}
-
-.home {
-    text-decoration: none;
-    color: #000
-}
-.home:hover {
-    text-decoration: underline;
-}
-
 </style>
 </head>
 <body>
@@ -413,57 +374,5 @@ select {
         <a href='https://github.com/hut/InstaGallery'>Gallery by InstaGallery (hut's fork)</a>
     </div>
     <?php print implode("\n",$moreHtml); ?>
-    <script>
-        $('a.swipebox').swipebox({
-            hideBarsDelay: -1,
-            afterOpen: function(){
-                var ui = $.swipebox.extend();
-                var close = $('#swipebox-close');
-                var fs = $('<i class="fa fa-arrows-alt"></i>');
-                fs.on('click',function(){
-                    var elem = $('#swipebox-overlay')[0];
-                    if (elem.requestFullscreen) {
-                        elem.requestFullscreen();
-                    } else if (elem.msRequestFullscreen) {
-                        elem.msRequestFullscreen();
-                    } else if (elem.mozRequestFullScreen) {
-                        elem.mozRequestFullScreen();
-                    } else if (elem.webkitRequestFullscreen) {
-                        elem.webkitRequestFullscreen();
-                    }
-                });
-
-                var pp = $('<i id="ppbutton" class="fa fa-play"></i>');
-                pp.on('click',function(e){
-                    var button = $(e.target);
-                    if(button.hasClass('fa-play')){
-                        button.removeClass('fa-play').addClass('fa-pause');
-                        button.attr('data-intid',window.setInterval(function(){ui.getNext()},5000));
-                    }else{
-                        button.removeClass('fa-pause').addClass('fa-play');
-                        window.clearInterval(button.attr('data-intid'));
-                        button.attr('data-intid','');
-                    }
-                });
-                var ctrlbox = $("<div id='ctrlbox'>");
-                ctrlbox.append(pp);
-                ctrlbox.append(fs);
-                close.after(ctrlbox);
-
-                // Play/pause button
-                // Spacebar/Enter advances
-                // big Fullscreen
-            },
-            afterClose: function(){
-                window.clearInterval($('#ppbutton').attr('data-intid'));
-            }
-        });
-
-        $(document).on('keyup',function(e){
-            if((e.keyCode == 32 || e.keyCode == 13) && $('#swipebox-overlay').length > 0){
-                $.swipebox.extend().getNext();
-            }
-        });
-    </script>
     </body>
 </html>
